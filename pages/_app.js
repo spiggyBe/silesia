3cca26c808561c78c59ce62650061d7946bf307d
SpiggyBe <bernacki.zbigniew@gmail.com>
2022-06-06 17:41:34 +0200

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import dynamic from "next/dynamic"
+
+const DynamicMetaTags = dynamic(() => import("../components/MetaTags/MetaTags"))
 import Layout from "../components/Layout/Layout"
 
 import "/styles/global.css"
@@ -5,6 +8,15 @@ import "/styles/global.css"
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <DynamicMetaTags
+        title={"tytul"}
+        keywords={"jakies keywordsy"}
+        description={"jakis opis"}
+        ogTitle={"tytul"}
+        ogType={"website"}
+        ogUrl={"https://silesiasites.pl"}
+        ogImage={"/images/logo.webp"}
+      />
       <Component {...pageProps} />
     </Layout>
   )
